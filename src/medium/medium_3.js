29d988e1860d829f17d529a472ae3cb86583020c
Natalie Yuziuk <59803404+nasyyu@users.noreply.github.com>
2021-11-27 13:51:19 -0500

    car data

--- src/medium/medium_3.js
+++ src/medium/medium_3.js
@@ -20,8 +20,8 @@ queries.
 export function searchHighPower(car_data, minHorsepower, minTorque) {
     let returnArray = [];
     for (let i = 0; i < car_data.length; i++) {
-        if (array[i].horsepower >= minHorsepower && array[i].torque >= minTorque) {
-            returnArray.push(array[i]);
+        if (car_data[i].horsepower >= minHorsepower && car_data[i].torque >= minTorque) {
+            returnArray.push(car_data[i]);
         }
     }
     returnArray = returnArray.sort((a, b) => b-a);
@@ -42,8 +42,8 @@ export function searchHighPower(car_data, minHorsepower, minTorque) {
 export function searchMpg(car_data, minCity, minHighway) {
     let returnArray = [];
     for (let i = 0; i < car_data.length; i++) {
-        if (array[i].city_mpg >= minCity && array[i].highway_mpg >= minHighway) {
-            returnArray.push(array[i]);
+        if (car_data[i].city_mpg >= minCity && car_data[i].highway_mpg >= minHighway) {
+            returnArray.push(car_data[i]);
         }
     }
     returnArray = returnArray.sort((a, b) => b-a);
@@ -60,7 +60,7 @@ export function searchMpg(car_data, minCity, minHighway) {
  * @returns {[]} array of cars
  */
 export function searchName(car_data, searchTerm) {
-    
+
 }
 
 
