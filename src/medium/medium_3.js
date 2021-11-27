74088b56da527e3fd1cf5e4f978a3040af7b53fb
Natalie Yuziuk <59803404+nasyyu@users.noreply.github.com>
2021-11-27 14:27:45 -0500

    sort id

--- src/medium/medium_3.js
+++ src/medium/medium_3.js
@@ -66,10 +66,21 @@ export function searchName(car_data, searchTerm) {
             x[i] = car_data[i];
         }
     }
+    x = x.sort(compareStringIndex(a,b,searchTerm));
     return x;
 }
 
-
+function compareStringIndex(a, b, searchString) {
+    if (a.indexOf(searchString) < b.indexOf(searchString)) {
+        return -1;
+    }
+    if (a.indexOf(searchString) == b.indexOf(searchString)) {
+        return 0;
+    }
+    if (a.indexOf(searchString) > b.indexOf(searchString)) {
+        return 1;
+    }
+} 
 
 /**
  * Find all cars made in the years asked for.
